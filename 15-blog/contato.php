ddd3e8ab4650cd383929c7e39b04d0a801cfb274
Gabriel Mattos <71332438+GabrielMattos93@users.noreply.github.com>
2022-02-11 21:35:55 -0300

    Separamos os Templates

---
+++ 15-blog/contato.php
@@ -0,0 +1,8 @@
+<?php
+require_once('templates/header.php'); 
+?>
+<body>
+    <h1>PÁGINA DE CONTATO</h1>
+    <?php
+require_once('templates/footer.php'); 
+?>
---
+++ 15-blog/templates/header.php
@@ -0,0 +1,31 @@
+<?php include_once ('helpers/url.php'); ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Estilos-->
+    <link rel="stylesheet" href="<?php echo $BASE_URL ?>/assets/css/style.css">
+     <!--Google Fonts-->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;600&family=Pacifico&display=swap" rel="stylesheet">
+
+    <title>Blog Codando</title>
+</head>
+<header>
+    <a href="<?php echo $BASE_URL ?>" id="logo"> 
+        <img src="<?php echo $BASE_URL ?>/assets/images/logo.png">
+    </a> 
+    <nav> 
+        <ul id="navbar">
+        <li><a href="<?php echo $BASE_URL ?>"class="nav-link"> Principal</a></li> 
+        <li><a href="#0"class="nav-link"> Categorias</a></li> 
+        <li><a href="#0"class="nav-link"> Sobre Nós</a></li> 
+        <li><a href="<?php echo $BASE_URL ?>contato.php"class="nav-link"> Contato</a></li> 
+        
+        </ul>
+    </nav>
+</header>
+<body>
